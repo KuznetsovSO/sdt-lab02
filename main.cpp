9bff84e733692aecc36086292af62eeadda18bdb
Кузнецов Степан <STEPENDIA@mpei.ru>
2018-05-31 16:16:03 +0300

    добавление деления

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+ #include <iostream>
 
  using namespace std;
-// учебная
+
  int main()
  {
  int a, b;
  cout << "Enter A and B: ";
  cin >> a >> b;
-     cout << "A + B = " << a + b << '\n';
-     cout << a*2;
-     cout<< a-b;
-
+ cout << "A + B = " << a + b << '\n'
+ << "A * 2 = " << a * 2 << '\n'
+ << "A - B = " << a - b << '\n'
+ << "A / B = " << a * b << '\n';
  }
